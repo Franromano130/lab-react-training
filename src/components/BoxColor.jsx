57709a566f6791c293543feac402c0a0a94aec66
Francisco Romano <franromano130@gmail.com>
2023-05-25 17:19:46 +0200

    commit training

---
+++ src/components/BoxColor.jsx
@@ -0,0 +1,13 @@
+function BoxColor(props) {
+  const myStyles = {
+    backgroundColor: `rgb(${props.r},${props.g},${props.b})`,
+  };
+
+  return (
+    <p style={myStyles} className="boxes">
+      rgb=({props.r},{props.g},{props.b})
+    </p>
+  );
+}
+
+export default BoxColor;
